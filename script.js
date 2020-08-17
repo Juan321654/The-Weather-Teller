7bc2bad9564211e6a22f3f20a8b8c402c92b13a5
Juan Camacho <kmilokmacho@hotmail.com>
2020-08-17 18:07:38 -0400

    user input works, wont display

--- script.js
+++ script.js
@@ -2,36 +2,41 @@
 //key weather fd4262e97d395dae4da9895a5f543573
 
 
-const userInput = addEventListener('click', ).value
 
-const getOptions = async() => {
-    // let city = 'bronx'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=bronx&appid=fd4262e97d395dae4da9895a5f543573`
+
+const getOptions = async(cityName) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fd4262e97d395dae4da9895a5f543573`
     try {
         const response = await axios.get(url)
         console.log(response)            //1
-        const allData = Object.keys(response.data)          //2
+        const allData = response.data          //2
         console.log(allData)                     //3       //5
     }catch (error) {
         console.log(`Error: ${error}`)
     }
     
-    function getValue (e) {         //12         e stands for event
-        e.preventDefault()
-        const optionValue = document.querySelector('#user-input').value  //13
-        console.log(optionValue)      //14•  this wont show on web dev tool yet
-        getOptins(optionValue)       //21
-    }
 }
 getOptions()
 
-function getInput (e) {
+// function getInput (e) {
+//     e.preventDefault()
+
+// }
+
+const inputButton = document.querySelector('#button')
+// console.log(inputButton)
+inputButton.addEventListener('click', getValue)
+
+// const userInput = addEventListener('click', cityName)
+
+function getValue (e) {         //12         e stands for event
     e.preventDefault()
-
+    const optionValue = document.querySelector('#user-input').value  //13
+    console.log(optionValue)      //14•  this wont show on web dev tool yet
+    getOptions(optionValue)
+    const cityLocation = document.querySelector('#location')
+    const cityDiv = document.createElement('p')
+    cityDiv.classList.add('#location')
+    cityDiv.append(optionValue)
+    console.log(cityLocation)
 }
-
-
-
-const cityName = document.querySelector('#user-input')
-console.log(cityName)
-cityName.addEventListener('click', getValue)
