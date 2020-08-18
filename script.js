5b1edd6d4feea8d4b07e4a0368f601513a4e5384
Juan Camacho <kmilokmacho@hotmail.com>
2020-08-18 10:58:47 -0400

    remove function works but removes all inputs

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const getOptions = async(cityName) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fd4262e97d395dae4da9895a5f543573&units=imperial`
     try {
         const response = await axios.get(url)
-        console.log(response)            
+        // console.log(response)            
         const allData = response.data          
-        console.log(allData)                     
+        // console.log(allData)   
+        // removeCity(getValue)                  
     }catch (error) {
         console.log(`Error: ${error}`)
     }
@@ -21,15 +22,25 @@ const inputButton = document.querySelector('#button')
 // console.log(inputButton)
 inputButton.addEventListener('click', getValue)
 
-
+//this function adds gets the user city input, and adds it to the screen
 function getValue (e) {         
     e.preventDefault()
     const optionValue = document.querySelector('#user-input').value  
     console.log(optionValue)      
     getOptions(optionValue)
-    const cityLocation = document.querySelector('#location')
+    const cityLocation = document.querySelector('#append')
     const cityDiv = document.createElement('p')
     cityDiv.classList.add('#location')
     cityLocation.append(optionValue)
-    console.log(cityLocation)
+    // console.log(cityLocation)
+    
+}
+
+
+function removeCity () {
+    const oldCity = document.querySelector('#append')
+    console.log(oldCity)
+    while(oldCity.firstChild){
+        oldCity.removeChild(oldCity.firstChild)
+    }
 }
